bf4bf0be985ef51fa9afcfbd95df3809abc52d87
Nurliman Diara <nurlimandiara@gmail.com>
2024-01-26 10:58:37 +0700

    init dashboard layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      unoptimized: true,
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "avatars.githubusercontent.com",
+        },
+        {
+          protocol: "https",
+          hostname: "avatar.vercel.sh",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
